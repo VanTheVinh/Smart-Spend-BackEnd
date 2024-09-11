c2001eeb39c0828cf06141054a19a279b4fe9643
Bin-Ne <vinhvlog2108@gmail.com>
2024-09-11 22:41:33 +0700

    Update requirement.txt, remove folder venv on origin

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'vinh_secret_key'
-    DB_NAME = "spendsmart_db"
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'   
+    DB_NAME = "railway"
     DB_USER = "postgres"
-    DB_PASSWORD = "210803"
-    DB_HOST = "localhost"
-    DB_PORT = "5432"
+    DB_PASSWORD = "nlSqZHxeaiYvjwMkYCFWSElHzWOhLSme"
+    DB_HOST = "junction.proxy.rlwy.net"
+    DB_PORT = "51474"
